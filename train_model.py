234907c70f8be06f6dd26eec9cfe479047a925dc
Tadios <tadiosmisganaw@gmail.com>
2026-05-01 14:10:26 +0300

    changeing model to linear regression

--- train_model.py
+++ train_model.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
-from sklearn.ensemble import RandomForestRegressor
+from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_absolute_error, r2_score, mean_squared_error
 import joblib
 import os
@@ -73,7 +73,7 @@ def run_ml_pipeline():
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
     
     # 7. Model Training
-    model = RandomForestRegressor(n_estimators=100, random_state=42)
+    model = LinearRegression()
     model.fit(X_train, y_train)
     
     # 8. Evaluation
